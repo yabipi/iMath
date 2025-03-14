ff96a2b8df1c906e358453b6aa7ed952409432ba
ku <kujunguo@gmail.com>
2025-03-14 22:59:59 +0800

    rename ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.imath"
+    namespace = "com.icool.coolmath"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.imath"
+        applicationId = "com.icool.coolmath"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/imath/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.imath
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
