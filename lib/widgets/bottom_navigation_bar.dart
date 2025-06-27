a19411c00250f0c559c47f72e6a5872728238fea
jgku <kujinyun@foxmail.com>
2025-06-27 21:57:22 +0800

    修改底部导航栏的bug

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -3,26 +3,30 @@ import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   // 使用 GetX 的控制器来管理状态
-  int _currentIndex = 0;
-
   CustomBottomNavigationBar({super.key}) {
     // 根据当前路由设置初始索引
   }
 
+  int _calculateSelectedIndex(BuildContext context) {
+    // final GoRouter route = GoRouter.of(context);
+    final currentRoute = GoRouterState.of(context);
+    String? path = currentRoute.path;
+    int _currentIndex = 0;
+    if (path == '/knowledge') {
+      _currentIndex = 0;
+    } else if (path == '/culture') {
+      _currentIndex = 1;
+    } else if (path == '/questions') {
+      _currentIndex = 2;
+    } else if (path == '/profile') {
+      _currentIndex = 3;
+    }
+
+    return _currentIndex;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentRoute = GoRouterState.of(context);
-    if (currentRoute == '/knowledge') {
-      _currentIndex = 0;
-    } else if (currentRoute == '/culture') {
-      _currentIndex = 1;
-    } else if (currentRoute == '/questions') {
-      _currentIndex = 2;
-    } else if (currentRoute == '/todo') {
-      _currentIndex = 3;
-    } else if (currentRoute == '/profile') {
-      _currentIndex = 4;
-    }
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed, // 明确设置为 fixed 类型
       items: const [
@@ -38,20 +42,18 @@ class CustomBottomNavigationBar extends StatelessWidget {
           icon: Icon(Icons.quiz),
           label: '题库',
         ),
-
         BottomNavigationBarItem(
           icon: Icon(Icons.person),
           label: '我的',
         ),
       ],
-      currentIndex: _currentIndex, // 动态绑定当前选中的索引
+      currentIndex: _calculateSelectedIndex(context), // 动态绑定当前选中的索引
       backgroundColor: Colors.white,
       selectedItemColor: Colors.blue, // 选中项的颜色
       unselectedItemColor: Colors.grey, // 未选中项的颜色
       showSelectedLabels: true, // 确保显示选中项的标签
       showUnselectedLabels: true, // 确保显示未选中项的标签
       onTap: (int index) {
-        _currentIndex = index; // 更新当前选中的索引
         // 根据索引跳转到不同的页面
         switch (index) {
           case 0:
