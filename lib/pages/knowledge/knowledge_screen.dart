68738268390c4acdfaf9b29a985c401106be43fe
ku <kujunguo@gmail.com>
2025-07-23 09:24:04 +0800

    dev

--- lib/pages/knowledge/knowledge_screen.dart
+++ lib/pages/knowledge/knowledge_screen.dart
@@ -36,62 +36,89 @@ class _KnowledgeScreenState extends ConsumerState<KnowledgeScreen> with Category
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
-        centerTitle: true, // 确保标题居中
-        flexibleSpace: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            SizedBox(
-              width: 0.5.sw, // 使用屏幕宽度的70%
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: '搜索...',
-                  prefixIcon: Icon(Icons.search, size: 20.sp),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(24.r),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
-                ),
-                onChanged: (value) {
-                  // 在这里处理搜索逻辑
-                },
-              )
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purple.shade300],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            Spacer(),
-          ],
+          ),
+        ),
+        title: TextField(
+          // controller: _searchController,
+          style: const TextStyle(color: Colors.white),
+          cursorColor: Colors.white,
+          decoration: const InputDecoration(
+            hintText: 'Search...',
+            hintStyle: TextStyle(color: Colors.white54),
+            border: InputBorder.none,
+          ),
+          onChanged: (value) {
+            // Perform search functionality here
+          },
         ),
       ),
+      // appBar: AppBar(
+      //   // title: const Text(''),
+      //   // centerTitle: true, // 确保标题居中
+      //   flexibleSpace: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Spacer(),
+      //       SizedBox(
+      //         width: 0.5.sw, // 使用屏幕宽度的70%
+      //         child: TextField(
+      //           decoration: InputDecoration(
+      //             hintText: '搜索...',
+      //             prefixIcon: Icon(Icons.search, size: 20.sp),
+      //             border: OutlineInputBorder(
+      //               borderRadius: BorderRadius.circular(24.r),
+      //             ),
+      //             contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
+      //           ),
+      //           onChanged: (value) {
+      //             // 在这里处理搜索逻辑
+      //           },
+      //         )
+      //       ),
+      //       Spacer(),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           context.push('/admin/addknow');
         },
         child: const Icon(Icons.add),
       ),
-      body: Column(
-        children: [
-          // 分类标签区域
-          CategoryPanel(onItemTap: (int categoryId) {onChangeCategory(categoryId);}),
-          Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.all(8),
-                itemCount: knowledges.length,
-                itemBuilder: (BuildContext context, int index){
-                  if (knowledges.length > 0) {
-                    return _buildKnowItem(knowledges[index]);
-                    // return ListTile(
-                    //   title: Text(knowledges[index].title),
-                    // );
-                  } else {
-                    return ListTile(
-                      title: Text('无效数据'),
-                    );
-                  }
-                }
-            )
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 分类标签区域
+            CategoryPanel(onItemTap: (int categoryId) {onChangeCategory(categoryId);}),
+            Expanded(
+                child: ListView.builder(
+                    padding: EdgeInsets.all(8),
+                    itemCount: knowledges.length,
+                    itemBuilder: (BuildContext context, int index){
+                      if (knowledges.length > 0) {
+                        return _buildKnowItem(knowledges[index]);
+                        // return ListTile(
+                        //   title: Text(knowledges[index].title),
+                        // );
+                      } else {
+                        return ListTile(
+                          title: Text('无效数据'),
+                        );
+                      }
+                    }
+                )
+            ),
+          ],
+        )
       ),
+
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
